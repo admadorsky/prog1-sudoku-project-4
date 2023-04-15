19652b255f290c74dfe4dd355ef6564d56fbd64f
admadorsky <127152790+admadorsky@users.noreply.github.com>
2023-04-14 23:43:03 -0400

    created main and sudoku files
    Sudoku file includes board generating code provided by professor.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Project 4: Sudoku by Anthony Madorsky, Muhammad Ali, Alexander Hennecke, and Joshua Park.
+# Sudoku board generating code adapted from a GeeksforGeeks article "Program for Sudoku Generator"
+# by Aarti_Rathi and Ankur Trisal https://www.geeksforgeeks.org/program-sudoku-generator/
+
+from sudoku import Sudoku
+
+# Driver code
+if __name__ == "__main__":
+    N = 9
+    K = 40
+    sudoku = Sudoku(N, K)
+    sudoku.fillValues()
+    sudoku.printSudoku()
